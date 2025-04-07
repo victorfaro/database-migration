90a753183a6ef4745bd3e70a6c143f3fefbdeab7
victorfaro <victor@nationgraph.com>
2025-04-07 00:09:25 -0300

    Added a migration.sh file with chained commands

---
+++ migration.sh
@@ -0,0 +1,6 @@
+make -f Reset.make reset-databases
+make -f InitialSetup.make initialize
+make -f ProdEducation.make dump_prod_education_all
+make -f ProdEducation.make restore_prod_education_all
+make -f Public.make dump_public_all
+make -f Public.make restore_public_all
