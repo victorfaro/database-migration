4c20037395131fb08973e80528ef6aa1b4a8522b
victorfaro <victor@nationgraph.com>
2025-05-13 20:26:43 -0300

    Added dbadmin

--- backup-data-final/roles.sql
+++ backup-data-final/roles.sql
@@ -4,4 +4,10 @@ CREATE ROLE dbwriter WITH PASSWORD 'nJwbirC03lxKoHX';
 
 CREATE ROLE dbreader WITH PASSWORD 'e5XHdFT4OCMZp9E';
 
+CREATE ROLE dbadmin WITH PASSWORD 'kUqHcCXWtDQGVfMnsvrdge';
+GRANT CONNECT TO DATABASE dev, prod TO dbwriter;
+GRANT CONNECT TO DATABASE dev, prod TO dbreader;
+GRANT CONNECT TO DATABASE dev, prod TO dbadmin;
+
 GRANT dbowner TO postgres;
+GRANT dbowner TO dbadmin;
